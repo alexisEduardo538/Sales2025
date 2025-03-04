dfe9df5587114dc22cc7795ba7ea8ff28bbbb154
alexisEduardo538 <ae736663@gmail.com>
2025-03-04 16:51:29 -0600

    Project

---
+++ Sales/Sales.mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Sales.mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Sales/Sales.mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Sales.mobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
